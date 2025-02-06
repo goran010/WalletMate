1fb128e883759d79ca835eff8def9a3050a256c8
Petar Car <petar.car135@gmail.com>
2025-02-06 16:10:51 +0100

    added 404 and 500 error pages

--- walletmate/walletmate/settings.py
+++ walletmate/walletmate/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-*48uqic-(*7s&-g@+hm81d^ng4k)(%)mh4(862n)6dv=0n^7jv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 
 
 # Application definition
