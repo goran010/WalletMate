999f369686007b199ff7cef1050719b0c28ed408
goran010 <giranmarkovic2008@gmail.com>
2025-02-07 10:03:10 +0100

    added host

--- walletmate/walletmate/settings.py
+++ walletmate/walletmate/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-*48uqic-(*7s&-g@+hm81d^ng4k)(%)mh4(862n)6dv=0n^7jv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["walletmate-1.onrender.com", "127.0.0.1", "localhost"]
+
 
 # Application definition
 
